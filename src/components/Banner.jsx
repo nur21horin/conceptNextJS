f8d0f8497914cf23152740fe390a0723ccee8242
Nur Mohammod <nurm98371@gmail.com>
2026-04-07 00:39:45 +0600

    ADd

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react'
-import {useROuter} from "next/navigation"
+import {useRouter} from "next/navigation"
 const Banner = () => {
     const router=useRouter();
   const handleBtn = () => {
