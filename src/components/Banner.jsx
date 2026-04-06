5f15f7e319f7d10c5725778e6a45ddfb670f762d
Nur Mohammod <nurm98371@gmail.com>
2026-04-06 21:57:14 +0600

    Add

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,27 @@
+"use client"
 import React from 'react'
-
+import {useROuter} from "next/navigation"
 const Banner = () => {
+    const router=useRouter();
+  const handleBtn = () => {
+    alert("clicked")
+    const password=prompt("Enter your password")
+    if (password == '1234') {
+ router.push("/dashboard")
+} else {
+  alert("Wrong password ❌")
+}
+  }
+
   return (
-    <div className='text-center space-y-5 bg-linear-60 to-sky-100 from-sky-300 '>
+    <div className='text-center space-y-5 bg-gradient-to-r from-sky-300 to-sky-100'>
       <h2 className='text-4xl font-bold'>Welcome to Dev story 🐳</h2>
-      <button className='px-4 py-2 bg-sky-500 text-white rounded cursor-pointer hover:bg-black duration-200'>Share story</button>
+      <button 
+        onClick={handleBtn} 
+        className='px-4 py-2 bg-sky-500 text-white rounded cursor-pointer hover:bg-black duration-200'
+      >
+        Share story
+      </button>
     </div>
   )
 }
