7a249a23396b8156b544ec08f7546f748fd2bfb7
Nur Mohammod <nurm98371@gmail.com>
2026-04-06 21:19:53 +0600

    Add

--- src/app/about/contact/page.jsx
+++ src/app/about/contact/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      <Title>Contacts</Title>
+      <Title>Contacts 01321526442</Title>
     </div>
   )
 }
---
+++ src/app/about/teams/page.jsx
@@ -0,0 +1,12 @@
+import Title from "@/components/Title";
+import React from "react";
+
+const Teams = () => {
+  return (
+    <div>
+      <Title>Teams member</Title>
+    </div>
+  );
+};
+
+export default Teams;
