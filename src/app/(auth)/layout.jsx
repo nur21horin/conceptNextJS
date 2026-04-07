20eacfea9241731fed91ed03f4e202480a03021e
Nur Mohammod <nurm98371@gmail.com>
2026-04-07 09:23:37 +0600

    Add

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -4,6 +4,11 @@ const AuthLayout = ({children}) => {
   return (
     <div className='flex min-h-screen gap-5'>
     <div className='flex-2'>{children}</div>
+    <div className='flex-1 bg-sky-600 p-5 flex justify-center items-center '>
+        <h2 className='text-5xl font-bold'>
+           Welcome for Authentication 
+        </h2>
+    </div>
       
     </div>
   )
