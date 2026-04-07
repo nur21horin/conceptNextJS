851d3a5ce2247e9941f73621bd6597e3bf2b5018
Nur Mohammod <nurm98371@gmail.com>
2026-04-07 09:13:14 +0600

    add

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -6,10 +6,10 @@ const DashboardLayout = ({children}) => {
     <div className='grid grid-cols-12 gap-5 min-h-screen'>
     <div className='col-span-3 border-r-2'>
         <h2>Navigation</h2>
-        <div className='flex flex-col gap-5'>
-        <Link href={"/add-story"}>Add story</Link>
-        <Link href={"/my-profile"}>My Profile</Link>
-        <Link href={"/settings"}>Settings</Link>
+        <div className='flex flex-col gap-5 mt-10'>
+        <Link  className='py-2 px-4 w-full bg-gray-600' href={"/dashboard/add-story"}>Add story</Link>
+        <Link className='py-2 px-4 w-full bg-gray-600' href={"/dashboard/my-profile"}>My Profile</Link>
+        <Link  className='py-2 px-4 w-full bg-gray-600' href={"/dashboard/settings"}>Settings</Link>
 
 
         </div>
